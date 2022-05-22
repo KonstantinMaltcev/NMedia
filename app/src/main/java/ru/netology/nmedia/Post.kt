b81a7482b027a8bc172af76a3e933c01b418241a
KonstantinMaltcev <Maltkost27@gmail.com>
2022-05-22 22:59:06 +0500

    Added log to logcat and refactoring

--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -6,8 +6,6 @@ data class Post(
     val content: String,
     val published: String,
     var likes: Int = 9999,
-    var reductionLike: String = "9.9K",
     var likedByMe: Boolean = false,
-    var shares: Int =999 ,
-    var reductionShare: String = "9.9K"
+    var shares: Int = 999
 )
